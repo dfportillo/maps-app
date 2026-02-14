7d4ad3198765708ea509001b31f7c5fdae8c7a1f
Daniel Portillo <danielportillo303@gmail.com>
2026-02-14 11:38:38 -0300

    map-app first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,23 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import "react-native-reanimated";
+import { useColorScheme } from "@/presentation/hooks/use-color-scheme";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name="loading/index" options={{animation:'none'}}/>
+        <Stack.Screen name="map/index" options={{animation:'none'}}/>
+        <Stack.Screen name="permissions/index" options={{animation:'none'}}/>
       </Stack>
-      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
---
+++ app/map/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function MappScreen() {
+  return (
+    <View>
+      <Text>MappScreen</Text>
+    </View>
+  );
+}
