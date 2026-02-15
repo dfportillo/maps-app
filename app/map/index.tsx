d4081f6fe4f7d6a07e60dcf00f3a09544412d926
Daniel Portillo <danielportillo303@gmail.com>
2026-02-15 14:42:33 -0300

    adding SDK Maps

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -1,12 +1,33 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 import React from "react";
 
 export default function MappScreen() {
   return (
-    <View>
-      <Text style={{
-        color:'white'
-      }}>MappScreen</Text>
+    <View style={styles.conteiner}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          //8.268225, -62.759109
+          latitude: 8.268225,
+          longitude: -62.759109,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  conteiner: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+    // backgroundColor:'blue',
+  },
+});
